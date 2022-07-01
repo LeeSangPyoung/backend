37650e1dc20c1a64a068d598c5db3575a18e45dc
강은미 <hv19962000@gmail.com>
2022-07-01 04:36:40 +0000

    LectureCategory 내부 파일 변경

---
+++ lecturecategory-domain/src/main/java/com/example/lecturecategory/domain/InterestCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.lecturecategory.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface InterestCategoryRepository extends CrudRepository<InterestCategory, Long>{
+    
+}
